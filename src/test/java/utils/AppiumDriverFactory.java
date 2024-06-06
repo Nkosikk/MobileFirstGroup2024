55b1d33228faf7c88803f1b52631d239486d2a5a
Cele <njceles@gmail.com>
2024-06-06 20:31:28 +0200

    completed the capability class

--- src/test/java/utils/AppiumDriverFactory.java
+++ src/test/java/utils/AppiumDriverFactory.java
@@ -1,7 +1,12 @@
 package utils;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class AppiumDriverFactory {
     public static AppiumDriver driver;
@@ -16,5 +21,23 @@ public class AppiumDriverFactory {
         capabilities.setCapability("autoAcceptAlert", true);
         capabilities.setCapability("appium:app", System.getProperty("user.dir") + "/src/main/Apps/ApiDemos-debug.apk");
 
+        try{
+            driver = new AndroidDriver(new URL(" http://127.0.0.1:4723/"),capabilities);
+//            driver = new AndroidDriver(new URL(" http://127.0.0.1:4723/wd/hub"),capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+    }
+
+    public static AppiumDriverFactory getInstanceOfAppiumDriverFactory(){
+        if(instanceOfAppiumDriverFactory==null)
+            instanceOfAppiumDriverFactory= new AppiumDriverFactory();
+        return instanceOfAppiumDriverFactory;
+    }
+
+    public AppiumDriver getDriver(){
+        return driver;
     }
 }
